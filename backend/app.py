da03d2b7dd64f33d501dc0d4bcac4adc8d410217
Stefanos Sophocleous <stefanossophocleous@Stefanoss-MacBook-Pro.local>
2024-05-11 16:21:17 -0700

    Aded Flake8, Git ignore and DB

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from Database.db import db
 
-app = Flask(__name__) # This correct?
 
+app = Flask(__name__)
+
+# Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./account.db'
+
+# Suppress a warning message
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'your_super_secret_key'
+
+
+# Initialize the database
+db.init_app(app)
+
+# Create tables before the first request using an alternative approach
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-     app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
